f8edd39cd0ea1f387dbd49456f830a3891ebfc8c
Robert Badea <badea.robert@ymail.com>
2020-04-04 20:58:30 +0300

    more tests with splash page

--- EinfachDeutsch/Views/SplashPageContentView.xaml.cs
+++ EinfachDeutsch/Views/SplashPageContentView.xaml.cs
@@ -14,7 +14,8 @@ namespace EinfachDeutsch
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SplashPageContentView : ContentView
     {
-        public int SweepValue { get; set; }
+        public int SweepValue { get; set; } = 0;
+        public int whoFirst = 0;
         public SplashPageContentView()
         {
             InitializeComponent();
@@ -30,31 +31,39 @@ namespace EinfachDeutsch
             float radius = (Math.Min(surfaceHeight, surfaceWidth) * 0.5f) - strokeWidth;
             canvas.Clear();
 
-            //outer circle
-            var outerPaint = new SKPaint
+            var leftPaint = new SKPaint
             {
                 Style = SKPaintStyle.Stroke, //stroke so that it traces the outline
                 Color = Color.Yellow.ToSKColor(), //make it the color red
                 StrokeWidth = strokeWidth
+            }; 
+            var rightPaint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke, //stroke so that it traces the outline
+                Color = Color.Red.ToSKColor(), //make it the color red
+                StrokeWidth = strokeWidth
             };
-
-            float equalDist = (surfaceWidth - radius) / 2;
 
             SKRect oval = new SKRect(strokeWidth, strokeWidth, surfaceWidth - strokeWidth, surfaceHeight - strokeWidth);
 
 
-            SweepValue += 1;
-            canvas.DrawArc(oval, 90, (SweepValue % 360), false, outerPaint);
-            PaintCanvasView.InvalidateSurface();
-            //canvas.DrawCircle(surfaceWidth / 2, surfaceHeight / 2, radius, outerPaint);
-
-            //inner circle
-            /*var innerPaint = new SKPaint()
+            SweepValue += 3;
+            if (SweepValue > 360) {
+                whoFirst = (whoFirst+1) % 2;
+                SweepValue = 0;
+            }
+            int idx = SweepValue > 180 ? 1 : 0;
+            List<SKPaint> colors = new List<SKPaint>(){ leftPaint, rightPaint };
+            if (whoFirst % 2 == 0)
             {
-                Style = SKPaintStyle.Fill,
-                Color = Color.LightBlue.ToSKColor(),
-            };
-            canvas.DrawCircle(surfaceWidth / 2, surfaceHeight / 2, radius, innerPaint);*/
+                canvas.DrawArc(oval, 90, (SweepValue % 360), false, colors[idx]);
+                canvas.DrawArc(oval, 90, ((SweepValue * (-1)) % 360), false, colors[(idx + 1) % 2]);
+            } else
+            {
+                canvas.DrawArc(oval, 90, ((SweepValue * (-1)) % 360), false, colors[idx]);
+                canvas.DrawArc(oval, 90, (SweepValue % 360), false, colors[(idx + 1) % 2]);
+            }
+            PaintCanvasView.InvalidateSurface();
         }
 
     }
